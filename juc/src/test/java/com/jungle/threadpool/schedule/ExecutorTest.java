29595e7562f3da76bd905aa014196a9bd3f5a511
Jungle <dgJungle_c@163.com>
2022-03-29 21:45:15 +0800

    试用

--- juc/src/test/java/com/jungle/threadpool/schedule/ExecutorTest.java
+++ juc/src/test/java/com/jungle/threadpool/schedule/ExecutorTest.java
@@ -1,10 +1,12 @@
 package com.jungle.threadpool.schedule;
 
+import cn.hutool.core.thread.ThreadFactoryBuilder;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 import java.util.Date;
+import java.util.Random;
 import java.util.concurrent.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -23,17 +25,27 @@ public class ExecutorTest {
         executor = new ThreadPoolExecutor(CORE_POOL_SIZE, MAX_POOL_SIZE, KEEP_ALIVE_TIME, TimeUnit.SECONDS,
                 new ArrayBlockingQueue<>(QUEUE_CAPACITY),
                 new ThreadPoolExecutor.CallerRunsPolicy());
+        ScheduledThreadPoolExecutor scheduledThreadPoolExecutor = new ScheduledThreadPoolExecutor(1,
+                ThreadFactoryBuilder.create().setNamePrefix("Image-").setDaemon(false).build());
+        scheduledThreadPoolExecutor.scheduleAtFixedRate(() -> {
+            System.out.println("=================================");
+            System.out.println("Thread size:" + executor.getPoolSize());
+            System.out.println("Active Thread size:" + executor.getActiveCount());
+            System.out.println("Number of Tasks:" + executor.getCompletedTaskCount());
+            System.out.println("Number of Tasks in Queue:" + executor.getQueue().size());
+            System.out.println("=================================");
+        }, 0, 1, TimeUnit.SECONDS);
 
     }
 
     @Test
     public void simpleScheduleTest() {
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 100; i++) {
             Runnable myRunnableDemo = () -> {
 
                 System.out.println(Thread.currentThread().getName() + " Start.Time:" + new Date());
                 try {
-                    TimeUnit.SECONDS.sleep(5);
+                    TimeUnit.SECONDS.sleep(new Random().nextInt(5));
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -44,6 +56,7 @@ public class ExecutorTest {
 
         executor.shutdown();
         while (!executor.isTerminated()) {
+
 
         }
         System.out.println("Finish all");
