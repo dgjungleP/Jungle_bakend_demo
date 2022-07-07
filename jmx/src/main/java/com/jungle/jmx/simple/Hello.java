509b06044e8da351118028511be623773788e738
dgjungleP <dgJungle_c@163.com>
2022-07-07 16:06:27 +0800

    Simple notification

--- jmx/src/main/java/com/jungle/jmx/simple/Hello.java
+++ jmx/src/main/java/com/jungle/jmx/simple/Hello.java
@@ -1,10 +1,15 @@
 package com.jungle.jmx.simple;
 
 
-public class Hello implements HelloMBean {
+import javax.management.AttributeChangeNotification;
+import javax.management.MBeanNotificationInfo;
+import javax.management.NotificationBroadcasterSupport;
+
+public class Hello extends NotificationBroadcasterSupport implements HelloMBean {
     public static final int DEFAULT_CACHE_SIZE = 200;
     public final String name = "Jungle";
     private int cacheSize = DEFAULT_CACHE_SIZE;
+    private int sequenceNumber = 1;
 
     @Override
     public void sayHello() {
@@ -28,7 +33,27 @@ public class Hello implements HelloMBean {
 
     @Override
     public synchronized void setCacheSize(int size) {
+        int oldSize = this.cacheSize;
+
+
         this.cacheSize = size;
         System.out.println("Cache size now is " + this.cacheSize);
+
+        // the template to use notification
+        AttributeChangeNotification notification = new AttributeChangeNotification(this, sequenceNumber++, System.currentTimeMillis(), "Cache size changed"
+                , "CacheSize", "int", oldSize, this.cacheSize);
+        sendNotification(notification);
+    }
+
+    @Override
+    public MBeanNotificationInfo[] getNotificationInfo() {
+        String[] types = {
+                AttributeChangeNotification.ATTRIBUTE_CHANGE
+        };
+        String className = AttributeChangeNotification.class.getName();
+        String description = "An attribute of this MBean has changed";
+        MBeanNotificationInfo info = new MBeanNotificationInfo(types, className, description);
+
+        return new MBeanNotificationInfo[]{info};
     }
 }
